13490261939af4666f16b8eea3193da557781f22
acme <1134418544@qq.com>
2019-12-18 23:25:54 +0800

    修改SG host

--- min-system-core/target/classes/thrift/Enums.thrift
+++
@@ -1,8 +0,0 @@
-namespace java com.acme.service.Enums
-
-enum OPERATE {
-    ADD,
-    SUB,
-    MUL,
-    DIV,
-}
--- min-system-core/target/classes/thrift/Exception.thrift
+++
@@ -1,6 +0,0 @@
-namespace java com.acme.service.Exception
-
-exception MinSystemException {
-    1:i32 code;
-    2:string message;
-}
